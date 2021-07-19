95190559bcb00b2da855fbe3cd20a61626b983e9
Shubh Nisar <shubh_ubuntu@shubh.ubuntu-Lenovo-ideapad-330S-15IKB>
2021-07-19 22:34:17 +0530

    Local Updates

--- portfolio/src/utils/internships.js
+++ portfolio/src/utils/internships.js
@@ -1,22 +1,39 @@
-export const internships = [{
+export const internships = [
+  {
+    phionike: {
+      name: "Phionike Studio",
+      duration: "3 mon",
+      position: "React & React Native Intern",
+      status: "green",
+      image: "/images/phionike.png",
+      linkedin: "https://www.linkedin.com/company/phionike-solutions/",
+      tech: [
+        "ReactJS",
+        "React Native",
+        "Custom npm library",
+        "Styled Components",
+      ],
+      projects: ["Supersnap"],
+    },
     sapio: {
-        name: "Sapio Analytica",
-        duration: "3 mon",
-        position: "Front End Developer Intern",
-        status: "green",
-        image: "/images/sapio.png",
-        linkedin: "https://www.linkedin.com/company/sapio-analytics/",
-        tech: ["ReactJS", "Context Hook", "CSS", "Styled Components"],
-        projects: ["Data Uploading Portal", "Chatbot"],
+      name: "Sapio Analytica",
+      duration: "3 mon",
+      position: "Front End Developer Intern",
+      status: "green",
+      image: "/images/sapio.png",
+      linkedin: "https://www.linkedin.com/company/sapio-analytics/",
+      tech: ["ReactJS", "Context Hook", "CSS", "Styled Components"],
+      projects: ["Data Uploading Portal", "Chatbot"],
     },
     dju: {
-        name: "DJU External Project",
-        duration: "3 mon",
-        position: "Front End Developer Intern",
-        status: "green",
-        image: "/images/unicode.png",
-        linkedin: "https://www.linkedin.com/company/djunicode/mycompany/",
-        tech: ["ReactJS", "Redux", "CSS", "Styled Components"],
-        projects: ["ProShop"],
-    }
-}];
+      name: "DJU External Project",
+      duration: "3 mon",
+      position: "Front End Developer Intern",
+      status: "green",
+      image: "/images/unicode.png",
+      linkedin: "https://www.linkedin.com/company/djunicode/mycompany/",
+      tech: ["ReactJS", "Redux", "CSS", "Styled Components"],
+      projects: ["ProShop"],
+    },
+  },
+];
